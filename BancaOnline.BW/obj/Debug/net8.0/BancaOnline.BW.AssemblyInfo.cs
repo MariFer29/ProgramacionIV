a98b6c37fa98b314bd11b7af974823efb3b656d6
Klein0102 <a.tony200301@gmail.com>
2025-11-16 14:27:53 -0600

    Codigo actualizado, Modula A, Modulo D y E finalizados, Modulo E funcional independientemente pero esperando para implementar otros modulos, Migracion del Modula A finalizado

--- BancaOnline.BW/obj/Debug/net8.0/BancaOnline.BW.AssemblyInfo.cs
+++ BancaOnline.BW/obj/Debug/net8.0/BancaOnline.BW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BancaOnline.BW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14cb36ad2a0d9d4c0eaefd7c4ea0b08bd4ccc5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3ee1de1c5eb6a6558f454214ac9e5b1b6abb0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BancaOnline.BW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BancaOnline.BW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
