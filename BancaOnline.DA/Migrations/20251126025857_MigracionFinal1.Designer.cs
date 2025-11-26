a3331359a59fcd307ca2b845dfa98be0dcff2222
Klein0102 <a.tony200301@gmail.com>
2025-11-25 21:01:56 -0600

    Migracion Final

--- BancaOnline.DA/Migrations/20251126025857_MigracionFinal1.Designer.cs
+++ BancaOnline.DA/Migrations/20251126025857_MigracionFinal1.Designer.cs
@@ -4,6 +4,7 @@ using BancaOnline.DA;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BancaOnline.DA.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251126025857_MigracionFinal1")]
+    partial class MigracionFinal1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
